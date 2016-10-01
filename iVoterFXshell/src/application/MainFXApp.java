e5c6667d78b4e976bf2a27896e96fe83fa911b3f
Christian Watts <civrev@gmail.com>
2016-10-01 18:30:00 -0400

    Update MainFXApp.java

--- iVoterFXshell/src/application/MainFXApp.java
+++ iVoterFXshell/src/application/MainFXApp.java
@@ -1,3 +1,7 @@
+//    This is a JavaFX shell of an iVoter program I got from my professor.
+//    I have stripped it of all functionality and removed the combo box.
+//    https://github.com/abegaz <- The original author of this program
+
 package application;
 import view.mainVoterViewController;
 import javafx.application.Application;
