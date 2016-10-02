a6c7db2683c85888e52d0b750f60371cf2c4e0ae
Christian Watts <civrev@gmail.com>
2016-10-01 20:01:39 -0400

    Add files via upload

---
+++ EasyiVoter/src/view/mainVoterViewController.java
@@ -0,0 +1,73 @@
+package view;
+
+import application.MainFXApp;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class mainVoterViewController {
+
+	Stage stage;
+	Scene scene;
+	Parent root;
+	//always reference main method, and build constructor
+	private MainFXApp main;
+	public void setMain(MainFXApp mainIn)
+	{
+	main=mainIn;
+	}
+
+	@FXML private Button displayButton;
+    @FXML private Button addButton;
+    @FXML private Button updateButton;
+
+    @FXML
+	void ClickButton(ActionEvent event) throws Exception {
+    	try{
+    	//finds what stage the button is in
+		stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+		//this gets the name of button
+		String temp = ((Node) event.getSource()).getId().toString();
+		//debugging
+		System.out.println("(Node) event.getSource()).getId().toString() = " + temp);
+		//this will allow for all buttons to go through one method, loading the
+		//right fxml file
+
+		switch (temp) {
+		case "displayButton":
+			root = FXMLLoader.load(getClass().getResource("/view/displayVoters.fxml"));
+			displayVotersController con1=new displayVotersController();
+			con1.setMain(main);
+			break;
+		case "addButton":
+			root = FXMLLoader.load(getClass().getResource("/view/addVoter.fxml"));
+			addVoterController con2=new addVoterController();
+			con2.setMain(main);
+			break;
+		case "updateButton":
+			root = FXMLLoader.load(getClass().getResource("/view/updateVoter.fxml"));
+			updateVoterController con3=new updateVoterController();
+			con3.setMain(main);
+			break;
+		default:
+			root = FXMLLoader.load(getClass().getResource("/view/mainVoterView.fxml"));
+			mainVoterViewController con4=new mainVoterViewController();
+			con4.setMain(main);
+			break;
+		}
+		//sets fxml file as a scene
+		scene = new Scene(root);
+		//loads the scene on top of whatever stage the button is in
+		stage.setScene(scene);
+	} catch (Exception e){
+		e.printStackTrace();
+	}
+    }
+
+
+}
---
+++ EasyiVoter/src/view/updateVoterController.java
@@ -0,0 +1,81 @@
+package view;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import application.MainFXApp;
+import application.iVoterDBConfig;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.Voter;
+
+public class updateVoterController {
+
+	Stage stage;
+	Scene scene;
+	Parent root;
+	//always reference main method, and build constructor
+	private MainFXApp main;
+	public void setMain(MainFXApp mainIn)
+	{
+	main=mainIn;
+	}
+
+	//This is all the stuff that needs to be worked with.
+    @FXML private TabPane tabPane;
+    @FXML private Button goButton;
+    @FXML private Button cancelButton;
+    @FXML private Button submitButton;
+    @FXML private Label fNameL;
+    @FXML private Label lNameL;
+    @FXML private Label addressL;
+    @FXML private Label countyL;
+    @FXML private Label stateL;
+    @FXML private Label zipL;
+    @FXML private Label phoneL;
+    @FXML private TextField voteridTF;
+    @FXML private TextField fNameTF;
+    @FXML private TextField lNameTF;
+    @FXML private TextField addressTF;
+    @FXML private TextField countyTF;
+    @FXML private TextField stateTF;
+    @FXML private TextField zipTF;
+    @FXML private TextField phoneTF;
+
+    @FXML
+	void ClickCancelButton(ActionEvent event) throws Exception {
+    	//finds what stage the button is in
+		stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+		//gets some fxml file
+		root = FXMLLoader.load(getClass().getResource("/view/mainVoterView.fxml"));
+		//sets fxml file as a scene
+		scene = new Scene(root);
+		//loads the scene on top of whatever stage the button is in
+		stage.setScene(scene);
+	}
+    @FXML
+	void ClickGoButton(ActionEvent event) {
+    	String vID = voteridTF.getText();
+    	//debugging
+    	System.out.println("ClickGoButton vID = " + vID);
+    }
+
+    @FXML
+    void ClickSubmitUpdateButton(ActionEvent event) {
+    	//debugging
+    	System.out.println("ClickSubmitUpdateButton (updateVoterController)");
+    }
+
+
+}
